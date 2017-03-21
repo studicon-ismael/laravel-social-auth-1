46e289f4e68245b87aada0e3922282f4340df09b
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2017-03-21 10:08:56 +0200

    Views updated

--- resources/views/attach.blade.php
+++ resources/views/attach.blade.php
@@ -1,7 +1,11 @@
 @if ($user = Auth::user())
     @foreach($socialProviders as $provider)
         @unless ($user->isAttached($provider->slug))
-            <a href="{{route('social.auth', [$provider->slug])}}" class="btn btn-lg waves-effect waves-light  btn-block {{$provider->slug}}">{{$provider->label}}</a>
+            <a
+                    href="{{ route('social.auth', [$provider->slug]) }}"
+                    class="btn btn-lg waves-effect waves-light btn-block {{ $provider->slug }}">
+                {{$provider->label}}
+            </a>
         @endunless
     @endforeach
 @endif
--- resources/views/buttons.blade.php
+++ resources/views/buttons.blade.php
@@ -1,3 +1,7 @@
 @foreach($socialProviders as $provider)
-    <a href="{{route('social.auth', [$provider->slug])}}" class="btn btn-lg waves-effect waves-light  btn-block {{ $provider->slug }}">{{$provider->label}}</a>
+    <a
+            href="{{ route('social.auth', [$provider->slug]) }}"
+            class="btn btn-lg waves-effect waves-light btn-block {{ $provider->slug }}">
+        {{ $provider->label }}
+    </a>
 @endforeach
