794d338435cd288d2b0279de72d598553bfea13f
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2017-03-28 16:24:09 +0300

    Testing environment setup

---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+namespace ZFort\SocialAuth\Test;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [\ZFort\SocialAuth\SocialAuthServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
