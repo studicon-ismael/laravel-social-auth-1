5e7a0ae954efed322065687388c2dbf6f25faaa5
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2017-05-28 01:15:32 +0300

    Callback route fix

--- resources/routes/routes.php
+++ resources/routes/routes.php
@@ -8,7 +8,7 @@ $this->app['router']->group(
     ],
     function ($router) {
         $router->get('social/{social}', 'SocialAuthController@getAccount')->name('auth');
-        $router->get('social/{social}/{social}callback', 'SocialAuthController@callback')->name('callback');
+        $router->get('social/{social}/callback', 'SocialAuthController@callback')->name('callback');
         $router->get('social/{social}/detach', 'SocialAuthController@detachAccount')->name('detach');
     }
 );
